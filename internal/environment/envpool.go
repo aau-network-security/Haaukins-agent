065c6e01b68dbea5f1d3bd03279f206f3e77d44f
Mikkel Høst Christiansen <mikkel.christiansen250996@gmail.com>
2022-10-13 15:47:16 +0200

    Added comments

--- internal/environment/envpool.go
+++ internal/environment/envpool.go
@@ -7,10 +7,12 @@ import (
 	"github.com/aau-network-security/haaukins-agent/internal/environment/lab"
 )
 
+// Returns a lab from the env pool if the lab tag exists in any of the environments
 func (ep *EnvPool) GetLabByTag(tag string) (*lab.Lab, error) {
 	for ke, e := range ep.Envs {
 		for kl, l := range e.Labs {
 			if l.Tag == tag {
+				// To make sure that lab is by reference and not by value
 				return ep.Envs[ke].Labs[kl], nil
 			}
 		}
