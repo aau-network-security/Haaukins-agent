d6a4fe8f07411c03c84da6b2bd4bfdf946b0d637
Mikkel Høst Christiansen <mikkel.christiansen250996@gmail.com>
2022-11-23 13:39:31 +0100

    Added ping

--- internal/agent/monitoring.go
+++ internal/agent/monitoring.go
@@ -1,3 +1,16 @@
 package agent
 
+import (
+	"context"
+
+	"github.com/aau-network-security/haaukins-agent/pkg/proto"
+)
+
 // TODO Heartbeat, resource monitoring and log monitoring
+
+func (a *Agent) Ping(ctx context.Context, req *proto.PingRequest) (*proto.PingResponse, error) {
+	if req.Ping == "ping" {
+		return &proto.PingResponse{Pong: "pong"}, nil
+	}
+	return &proto.PingResponse{Pong: "the hell is that kind of ping?"}, nil
+}
